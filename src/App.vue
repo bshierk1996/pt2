a77bbdf2bb90b6c5ec7ae57ae66f313d39f25684
John Dickinson <es@empathys-MacBook-Pro.local>
2020-01-01 18:56:50 -0500

    Config in-app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 
   <div id="app">
-     <HelloWorld />
+    <HelloWorld />
     <router-view></router-view>
     <!-- <signup /> -->
   </div>
@@ -9,15 +9,40 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import * as firebase from 'firebase';
 
 export default {
   name: 'app',
-  components: { HelloWorld }
+  components: { HelloWorld },
+  mounted() {
+    this.initFirebase();
+  },
+  data() {
+    return {
+      fbConfig: {
+        apiKey: "AIzaSyAmtuksKKkhZlWGl57BbAwjNgViH2p0xfA",
+        authDomain: "keisha-fitness.firebaseapp.com",
+        databaseURL: "https://keisha-fitness.firebaseio.com",
+        projectId: "keisha-fitness",
+        storageBucket: "keisha-fitness.appspot.com",
+        messagingSenderId: "665776943970",
+        appId: "1:665776943970:web:baa2597e8b5f2cc360f23b",
+        measurementId: "G-MWYYGCRG0M"
+      }
+    }
+  },
+  methods: {
+    initFirebase() {
+      // Initialize Firebase
+      firebase.initializeApp(this.fbConfig);
+      firebase.analytics();
+    }
+  }
 }
 </script>
 
 <style>
-*{
+* {
   box-sizing: border-box;
   padding: 0;
   margin: 0;
